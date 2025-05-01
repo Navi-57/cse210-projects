ff5dc6c1527a18d079c438d7bf42f30c25d0d02a
Navi-57 <navisr57@gmail.com>
2025-05-01 09:56:38 -0600

    added functions

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,8 +2,28 @@ using System;
 
 class Program
 {
+    static double AddNumbers(double a)
+    {
+        return a + a;
+    }
+
+    static void DisplayGreeting()
+    {
+        Console.WriteLine("Hello.");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine(AddNumbers(3, 4));
+        List<int> myNumbers = new List<int>();
+        myNumbers.Add(7);
+        myNumbers.Add(44);
+        myNumbers.Add(27);
+
+        foreach(int i in myNumbers)
+        {
+            Console.WriteLine(AddNumbers(i));
+        }
+        DisplayGreeting();
     }
 }
