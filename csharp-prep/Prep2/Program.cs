32f6b84112c5f4a417d5578e6c95659f5dee7427
Navi-57 <navisr57@gmail.com>
2025-05-01 09:12:18 -0600

    did assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Formats.Asn1;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percent = Console.ReadLine();
+        int number = int.Parse(percent);
+        string letter = "";
+        string passed = "";
+        if (number >= 90)
+        {
+            letter = "A";
+            passed = "you passed!";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+            passed = "you Passed!";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+            passed = "you passed!";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+            passed = "you did not pass.";
+        }
+        else
+        {
+            letter = "F";
+            passed = "you did not pass.";
+        }
+        if (letter == "A")
+        {
+            Console.WriteLine($"Your grade is an {letter} and {passed}");
+        }
+        else if (letter == "F")
+        {
+            Console.WriteLine($"Your grade is an {letter} and {passed}");
+        }
+        else 
+        {
+            Console.WriteLine($"Your grade is a {letter} and {passed}");
+        }
     }
 }
